c0c1e74ccafe7d00648ff8ac9181094689c0b51e
teodegiu99 <dev.matteodegiuseppe@gmail.com>
2026-04-25 15:45:35 +0200

    better ui

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    // Aggiungi questo per permettere a Next.js di accettare connessioni dal tuo dominio
+    allowedDevOrigins: ['greatsume.com', 'www.greatsume.com'],
+  },
+  images: {
+    // Per risolvere il warning sulla qualità delle immagini
+    qualities: [100, 75],
+  },
+};
 
 export default nextConfig;
